846664ff2e5306aca66a192da0ef75ae798b4976
ColinLee1998 <colinlee12171998@gmail.com>
2022-02-27 01:17:18 +0800

    20220227 設置登入機制

--- session/login_output.php
+++ session/login_output.php
@@ -1,5 +1,19 @@
 <?php
     session_start();
     unset($_SESSION['customer']);
-    $pdo = new PDO('mysql:host=localhost; dbname=shop; charset=utf8', '')
+    $pdo = new PDO('mysql:host=localhost; dbname=shop; charset=utf8', 'colin', '19981217');
+    $sql = $pdo->prepare('SELECT * FROM customer WHERE login = ? AND `password` = ?');
+    $sql->execute([$_REQUEST['login'], $_REQUEST['password']]);
+    foreach ($sql->fetch() as $row) {
+        $_SESSION['customer'] = [ 
+            'id'=>$row['id'], 'name'=>$row['name'], 'address'=>$row['address'], 
+            'login'=>$row['login'], 'password'=>$row['password']
+        ];
+    }
+
+    if (isset($_SESSION['customer'])) {
+        echo '您好', $_SESSION['customer']['name'], '、歡迎光臨。';
+    } else {
+        echo '登入的帳號密碼錯誤。';
+    }
 ?>
